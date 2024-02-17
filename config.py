921f859f1d3006dc30f247a88dbf56fde8b673a5
zzmvp-1 <85637429+zzmvp-1@users.noreply.github.com>
2024-02-17 23:27:04 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 url = "https://www.wjx.cn/vm/QDmCDYI.aspx#"
 epochs = 400
-# 题项比例
+# 题项比例，确保选项数量和数组长度一致
 prob = {
     1: [1, 1],
     2: [1, 1, 1, 2, 2, 0],
@@ -12,8 +12,8 @@ prob = {
 answerList = {
     6: ["123", "12"]
 }
-# IP API提取链接
-api = "https://service.ipzan.com/core-extract?num=1&no=20240204136159871960&minute=1&format=txt&protocol=1&pool=quality&mode=whitelist&secret=l03fmb8u1b8r8"
+# IP API提取链接 https://xip.ipzan.com/ 这个每周都有几百个免费的IP代理领取
+api = ""
 # User-Agent库
 UA = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0",
       "Mozilla/5.0 (Linux; Android 10; SEA-AL10 Build/HUAWEISEA-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/4313 MMWEBSDK/20220805 Mobile Safari/537.36 MMWEBID/9538 MicroMessenger/8.0.27.2220(0x28001B53) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64",
