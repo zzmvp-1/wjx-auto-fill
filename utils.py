008706c2f17aa1144f66541cda67a5c9dfc1257e
zzmvp-1 <85637429+zzmvp-1@users.noreply.github.com>
2024-03-02 16:29:39 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -73,11 +73,12 @@ def single_matrix_scale(driver, id, prob, idx, num):
     for i in range(1, int(q_len) + 1):
         xpath = '//*[@id="drv{}_{}"]/td'.format(id, i)
         answers = driver.find_elements(By.XPATH, xpath)
-        p = preprocess_prob(prob.get(idx), len(answers) - 1)
         # 判断是浏览器还是QQ、微信
         if num == 0:
+            p = preprocess_prob(prob.get(idx), len(answers) - 1)
             choice = numpy.random.choice(a=numpy.arange(1, len(answers)), p=p)
         else:
+            p = preprocess_prob(prob.get(idx), len(answers))
             choice = numpy.random.choice(a=numpy.arange(0, len(answers)), p=p)
         xpath = '//*[@id="drv{}_{}"]/td[{}]'.format(id, i, choice + 1)
         time.sleep(0.5)
@@ -92,16 +93,22 @@ def multi_matrix_scale(driver, id, prob, idx, num):
     for i in range(1, int(q_len)+1):
         xpath = '//*[@id="drv{}_{}"]/td'.format(id, i)
         answers = driver.find_elements(By.XPATH, xpath)
-        p = preprocess_prob(prob.get(idx), len(answers)-1)
-        # 多选的数量
-        # 概率为0的选项个数
-        count = sum(1 for i in p if i == 0)
+        # 判断是浏览器还是QQ、微信
         if num == 0:
+            p = preprocess_prob(prob.get(idx), len(answers) - 1)
+            # 多选的数量
+            # 概率为0的选项个数
+            count = sum(1 for i in p if i == 0)
             n = random.randint(1, len(answers) - 1 - count)
             q_selects = numpy.random.choice(a=numpy.arange(1, len(answers)), size=n, replace=False, p=p)
         else:
+            p = preprocess_prob(prob.get(idx), len(answers))
+            # 多选的数量
+            # 概率为0的选项个数
+            count = sum(1 for i in p if i == 0)
             n = random.randint(1, len(answers) - count)
             q_selects = numpy.random.choice(a=numpy.arange(0, len(answers)), size=n, replace=False, p=p)
+
         for q_select in q_selects:
             xpath = '//*[@id="drv{}_{}"]/td[{}]'.format(id, i, q_select+1)
             driver.find_element(By.XPATH, xpath).click()
